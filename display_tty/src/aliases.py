2bb75adf6503298db921aa966c10827852b4d5f6
Henry LETELLIER <henry.letellier@epitech.eu>
2025-11-06 08:50:56 +0100

    [ADD] a alias file to store variable aliases to ease calls

---
+++ display_tty/src/aliases.py
@@ -0,0 +1,60 @@
+from .constants import OUT_TTY, OUT_STRING, OUT_FILE, OUT_DEFAULT, KEY_OUTPUT_MODE, KEY_PRETTIFY_OUTPUT, KEY_PRETTIFY_OUTPUT_IN_BLOCKS, KEY_ANIMATION_DELAY, KEY_ANIMATION_DELAY_BLOCKY
+
+TMP = None  # linter bypass for a false positive of statement has no effect
+
+
+# Output Modes
+"""
+@var OSTRING
+@brief Alias for OUT_STRING constant.
+"""
+OSTRING = OUT_STRING
+
+"""
+@var ODEFAULT
+@brief Alias for OUT_DEFAULT constant.
+"""
+ODEFAULT = OUT_DEFAULT
+
+"""
+@var OFILE
+@brief Alias for OUT_FILE constant.
+"""
+OFILE = OUT_FILE
+
+"""
+@var OTTY
+@brief Alias for OUT_TTY constant.
+"""
+OTTY = OUT_TTY
+
+# Configuration Keys
+"""
+@var KOUTPUT_MODE
+@brief Alias for KEY_OUTPUT_MODE constant.
+"""
+KOUTPUT_MODE = KEY_OUTPUT_MODE
+
+"""
+@var KPRETTIFY_OUTPUT
+@brief Alias for KEY_PRETTIFY_OUTPUT constant.
+"""
+KPRETTIFY_OUTPUT = KEY_PRETTIFY_OUTPUT
+
+"""
+@var KANIMATION_DELAY
+@brief Alias for KEY_ANIMATION_DELAY constant.
+"""
+KANIMATION_DELAY = KEY_ANIMATION_DELAY
+
+"""
+@var KANIMATION_DELAY_BLOCKY
+@brief Alias for KEY_ANIMATION_DELAY_BLOCKY constant.
+"""
+KANIMATION_DELAY_BLOCKY = KEY_ANIMATION_DELAY_BLOCKY
+
+"""
+@var KPRETTIFY_OUTPUT_IN_BLOCKS
+@brief Alias for KEY_PRETTIFY_OUTPUT_IN_BLOCKS constant.
+"""
+KPRETTIFY_OUTPUT_IN_BLOCKS = KEY_PRETTIFY_OUTPUT_IN_BLOCKS
